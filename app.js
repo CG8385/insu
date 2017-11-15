43eff1a1a977862cf3e1b0350e427c647e5cf48b
Chun Wang <chunw@surveymonkey.com>
2017-11-14 20:58:55 -0500

    test ci

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apis);
-app.use('/', routes);
+// app.use('/', routes);
 app.use('/users', users);
 app.use('/wechat', wechat);
 
