138fd494314473263617148a0a70d5259eccfd45
Chun Wang <chunw@surveymonkey.com>
2017-12-03 23:21:21 -0500

    test

--- utils/database.js
+++ utils/database.js
@@ -5,6 +5,7 @@ var config = require('../common.js').config();
 var mongoose = require('mongoose');
 
 var connection = mongoose.createConnection(config.mongodb_server);
+mongoose.Promise = require('q').Promise;
 
 connection.on('error', function (err) {
     console.log(err);
