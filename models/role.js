6c7c6d2a0511a354f243070ee53d1866db02c4f2
Chun Wang <chunw@surveymonkey.com>
2018-02-07 21:34:43 -0500

    add roles

--- models/role.js
+++ models/role.js
@@ -13,14 +13,7 @@ var schema = new mongoose.Schema({
         append: { type: Boolean, default: false },
         aprove: { type: Boolean, default: false },
         pay: { type: Boolean, default: false },
-    },
-    policy: {
-        edit: { type: Boolean, default: false }, 
-        view: { type: Boolean, default: false },
-        delete: { type: Boolean, default: false },
-        append: { type: Boolean, default: false },
-        aprove: { type: Boolean, default: false },
-        pay: { type: Boolean, default: false },
+        export: { type: Boolean, default: false },
     },
     lifePolicy: {
         edit: { type: Boolean, default: false }, 
@@ -29,6 +22,7 @@ var schema = new mongoose.Schema({
         append: { type: Boolean, default: false },
         aprove: { type: Boolean, default: false },
         pay: { type: Boolean, default: false },
+        export: { type: Boolean, default: false },
     },
     dealerPolicy: {
         edit: { type: Boolean, default: false }, 
@@ -38,38 +32,45 @@ var schema = new mongoose.Schema({
         aprove: { type: Boolean, default: false },
         pay: { type: Boolean, default: false },
         import: { type: Boolean, default: false },
+        export: { type: Boolean, default: false },
     },
     imagePolicy:{
         view: { type: Boolean, default: false },
-        process: { type: Boolean, default: false }
+        edit: { type: Boolean, default: false },
+        export: { type: Boolean, default: false },
     },
     client: {
         edit: { type: Boolean, default: false }, 
         view: { type: Boolean, default: false },
         delete: { type: Boolean, default: false },
         append: { type: Boolean, default: false },
-        aprove: { type: Boolean, default: false }
+        aprove: { type: Boolean, default: false },
+        export: { type: Boolean, default: false },
     },
     employee: {
         edit: { type: Boolean, default: false }, 
         view: { type: Boolean, default: false },
         delete: { type: Boolean, default: false },
-        append: { type: Boolean, default: false }
+        append: { type: Boolean, default: false },
+        export: { type: Boolean, default: false },
     },
     company: {
         edit: { type: Boolean, default: false }, 
         view: { type: Boolean, default: false },
         delete: { type: Boolean, default: false },
-        append: { type: Boolean, default: false }
+        append: { type: Boolean, default: false },
+        export: { type: Boolean, default: false },
     },
     organization: {
         edit: { type: Boolean, default: false }, 
         view: { type: Boolean, default: false },
         delete: { type: Boolean, default: false },
-        append: { type: Boolean, default: false }
+        append: { type: Boolean, default: false },
+        export: { type: Boolean, default: false },
     },
     log: {
-        view: { type: Boolean, default: false }
+        view: { type: Boolean, default: false },
+        export: { type: Boolean, default: false },
     },
 });
 
