2f214c65c3e1603a12111ec7153d51a88c26b6f0
Chun Wang <chunw@surveymonkey.com>
2018-02-08 22:03:25 -0500

    fix

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ if (app.get('env') === 'development') {
 
 app.use(log4jsLogger);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
 app.use(cookieParser());
 app.use(session({
   name: 'insu',
