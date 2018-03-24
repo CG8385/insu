f49a1b11ad006af87d16d253cdf4bcd2ca65e411
Chun Wang <chunw@surveymonkey.com>
2018-03-24 11:47:32 -0400

    fix

--- routes/migrate.js
+++ routes/migrate.js
@@ -20,7 +20,7 @@ var User = Promise.promisifyAll(require('../models/user.js')(db));
 var Role = Promise.promisifyAll(require('../models/role.js')(db));
 
 router.get('/step1', asyncMiddleware(async (req, res, next) => {
-    Organization.remove({_id:'5ab64e90c7cbdc0ce9b45117'});
+    Organization.remove({name:'红叶保险代理法人机构'});
     Organization.updateAsync({name:'红叶保险代理有限公司法人机构'}, {level:'一级机构', area_code:'0'});
     res.json("finish");
 }));
