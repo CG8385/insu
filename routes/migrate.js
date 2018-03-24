edac02cb7c45e1f51b65e552a85c38b487f54e0e
Chun Wang <chunw@surveymonkey.com>
2018-03-24 14:55:28 -0400

    fix

--- routes/migrate.js
+++ routes/migrate.js
@@ -18,11 +18,28 @@ var Client = require('../models/client.js')(db);
 var Organization = Promise.promisifyAll(require('../models/organization.js')(db));
 var User = Promise.promisifyAll(require('../models/user.js')(db));
 var Role = Promise.promisifyAll(require('../models/role.js')(db));
+var locations = require('../pca.json');
 
 router.get('/step1', asyncMiddleware(async (req, res, next) => {
     await Organization.removeAsync({name:'红叶保险代理法人机构'});
     await Organization.updateAsync({name:'红叶保险代理有限公司法人机构'}, {level:'一级机构', area_code:'0'});
     let level1 = await Organization.find({name:'红叶保险代理有限公司法人机构'}).exec();
+    let level2 = new Organization({name:'江苏省分公司', level:'二级机构', province:'江苏省', area_code:'032'})
+    level2 = Promise.promisifyAll(level2);
+    await level2.saveAsync();
+    level2 = await Organization.findOne({name:'江苏省分公司'}).exec();
+    await Organization.updateAsync({name:'苏州分公司'}, {level:'三级机构', province:'江苏省', city:'苏州市', area_code:'03205', parent: level2._id});
+    await Organization.updateAsync({name:'徐州分公司'}, {level:'三级机构', province:'江苏省', city:'徐州市', area_code:'03203', parent: level2._id});
+    await Organization.updateAsync({name:'徐州睢宁营业部'}, {level:'三级机构', province:'江苏省', city:'徐州市',district:'睢宁县', area_code:'0320324', parent: level2._id});
+    let level3 = new Organization({name:'宿迁分公司'}, {level:'三级机构', province:'江苏省', city:'宿迁市', area_code:'03213', parent: level2._id})
+    level3 = Promise.promisifyAll(level3);
+    level3.saveAsync();
+    level3 = new Organization({name:'连云港分公司'}, {level:'三级机构', province:'江苏省', city:'连云港市', area_code:'03207', parent: level2._id})
+    level3 = Promise.promisifyAll(level3);
+    level3.saveAsync();
+    level3 = new Organization({name:'南京分公司'}, {level:'三级机构', province:'江苏省', city:'南京市', area_code:'03201', parent: level2._id})
+    level3 = Promise.promisifyAll(level3);
+    level3.saveAsync();
     res.json("finish");
 }));
 
