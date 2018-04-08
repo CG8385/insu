96221110fb88ef24692ef7a29ce7e1bb298c1023
Chun Wang <chunw@surveymonkey.com>
2018-04-07 23:54:28 -0400

    fix

--- routes/migrate.js
+++ routes/migrate.js
@@ -243,15 +243,15 @@ router.get('/correction', asyncMiddleware(async (req, res, next) => {
     wrongLevel2 = Promise.promisifyAll(wrongLevel2);
     await wrongLevel2.removeAsync();
 
-    let wrongLevel3 = await Company.findOne({level:'三级', name:'中国人民财产保险股份有限公司江苏分公司'}).exec();
-    level3 = await Company.findOne({level:'三级', name:'中国人民财产保险股份有限公司苏州中心支公司'}).exec();
-    await Policy.update({level3_company: wrongLevel3._id}, {level3_company: level3._id, company: level3._id}, {multi: true});
+    // let wrongLevel3 = await Company.findOne({level:'三级', name:'中国人民财产保险股份有限公司江苏分公司'}).exec();
+    // level3 = await Company.findOne({level:'三级', name:'中国人民财产保险股份有限公司苏州中心支公司'}).exec();
+    // await Policy.update({level3_company: wrongLevel3._id}, {level3_company: level3._id, company: level3._id}, {multi: true});
 
-    wrongLevel3 = await Company.findOne({level:'三级', name:'中国人民财产保险股份有限公司南京支公司第一营业部'}).exec();
-    let correctLevel3 = await Company.findOne({level:'三级', name:'中国人民财产保险股份有限公司南京支公司'}).exec();
-    wrongLevel3.level = '四级';
-    wrongLevel3.parent = correctLevel3._id;
-    await Policy.update({level3_company: wrongLevel3._id}, {level3_company: correctLevel3._id, level3_company: wrongLevel3._id}, {multi: true});
+    // wrongLevel3 = await Company.findOne({level:'三级', name:'中国人民财产保险股份有限公司南京支公司第一营业部'}).exec();
+    // let correctLevel3 = await Company.findOne({level:'三级', name:'中国人民财产保险股份有限公司南京支公司'}).exec();
+    // wrongLevel3.level = '四级';
+    // wrongLevel3.parent = correctLevel3._id;
+    // await Policy.update({level3_company: wrongLevel3._id}, {level3_company: correctLevel3._id, level3_company: wrongLevel3._id}, {multi: true});
 
     res.send("done");
 }));
