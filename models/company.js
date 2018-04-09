7beeae07c08426008f213738f690f44c879671e8
Chun Wang <chunw@surveymonkey.com>
2018-04-08 23:00:56 -0400

    fix

--- models/company.js
+++ models/company.js
@@ -13,7 +13,6 @@ var schema = new mongoose.Schema({
     parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
     rates: [{ set_at: { type: Date }, mandatory_income: Number, mandatory_payment: Number, commercial_income: Number, commercial_payment: Number, tax_income: Number, tax_payment: Number, other_income: Number, other_payment: Number }],
     py: String,
-
     province: String,
     city: String,
     district: String,
