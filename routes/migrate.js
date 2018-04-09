466acb7c8090a7bddde31ccf3751bc7847862ef1
Chun Wang <chunw@surveymonkey.com>
2018-04-08 22:51:00 -0400

    fix

--- routes/migrate.js
+++ routes/migrate.js
@@ -25,42 +25,10 @@ router.get('/roles', asyncMiddleware(async (req, res, next) => {
 }));
 
 router.get('/step4', asyncMiddleware(async (req, res, next) => {
-    // let level3 = await Organization.findOne({ name: '苏州分公司' }).exec();
-    // await Organization.updateAsync({ name: '苏州车险业务部' }, { level: '四级机构', parent: level3._id });
-
-
-    // let level4 = await Organization.findOne({ name: '睢宁分公司个险部' }).exec();s
-    // await Organization.updateAsync({ name: '个险-睢宁-公司' }, { level: '五级机构', province: '江苏省', city: '徐州市', district: '睢宁县', area_code: '0320324', parent: level4._id });
-    let clients = await Client.find({}).exec();
-    for (let i = 0; i < clients.length; i++) {
-        let client = clients[i];
-        client = Promise.promisifyAll(client);
-        client.organization = client.level5_org;
-        await client.saveAsync();
-    }
-    // await Organization.updateAsync({ name: '个险-睢宁-团队' }, { level: '五级机构', province: '江苏省', city: '徐州市', district: '睢宁县', area_code: '0320324', parent: level4._id });
-    // await Organization.updateAsync({ name: '个险-睢宁-同业' }, { level: '五级机构', province: '江苏省', city: '徐州市', district: '睢宁县', area_code: '0320324', parent: level4._id });
-
-
-    // await Organization.updateAsync({ name: '财产险' }, { level: '四级机构', province: '江苏省', city: '徐州市', district: '睢宁县', area_code: '0320324', parent: level3._id });
-    // level4 = await Organization.findOne({ name: '财产险' }).exec();
-    // let level5 = new Organization({ name: '睢宁分公司财产险1部', level: '五级机构', province: '江苏省', city: '徐州市', district: '睢宁县', area_code: '0320324', parent: level4._id })
-    // level5 = Promise.promisifyAll(level5);
-    // await level5.saveAsync();
-    // let level2_id = level3.parent;
-    // let level2 = await Organization.findOne({ _id: level2_id }).exec();
-    // let level1_id = level2.parent;
-    // await Client.updateAsync({ organization: level4._id }, { level1_org: level1_id, level2_org: level2_id, level3_org: level3._id, level4_org: level4._id, level5_org: level5._id, organization: level5._id }, { multi: true });
-
-    // let level3 = await Organization.findOne({ name: '徐州睢宁分公司' }).exec();
-    // let level4 = await Organization.findOne({ name: '睢宁分公司车险部' }).exec();
-    // let level5 = await Organization.findOne({ name: '睢宁分公司车险1部' }).exec();
-    // let ooo = await Organization.findOne({ name: '车险业务部' }).exec();
-    // let level2_id = level3.parent;
-    // let level2 = await Organization.findOne({ _id: level2_id }).exec();
-    // let level1_id = level2.parent;
-    // await Client.updateAsync({ organization: ooo._id }, { level1_org: level1_id, level2_org: level2_id, level3_org: level3._id, level4_org: level4._id, level5_org: level5._id, organization: level5._id }, { multi: true });
-    // await Policy.updateAsync({ organization: ooo._id }, {organization: level5._id}, { multi: true });
+    let user = await User.findOne({name:'管理员'}).exec();
+    let org = await Organization.findOne({name:'红叶保险代理有限公司法人机构'}).exec();
+    user.org = org._id;
+    user.save();
 
     res.json('done');
 }));
