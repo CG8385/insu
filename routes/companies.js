ecb4cedca8e0ea6d518f8876f03e588d21baa326
Chun Wang <chunw@surveymonkey.com>
2018-04-08 23:02:44 -0400

    fix

--- routes/companies.js
+++ routes/companies.js
@@ -309,8 +309,8 @@ router.get('/sub/:parentId', asyncMiddleware(async (req, res, next) => {
     }
   }
   // res.status(200).json(query);
-  let companies = await Company.find({}).sort({py: -1}).exec();
-  // let companies = await Company.find(query).sort({py: -1}).exec();
+  // let companies = await Company.find({}).sort({py: -1}).exec();
+  let companies = await Company.find(query).sort({py: -1}).exec();
   res.status(200).json(companies);
 }));
 
