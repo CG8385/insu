fe11f841ad5ed654b9ca57fe02d796a485e22fd7
Chun Wang <chunw@surveymonkey.com>
2018-04-17 21:37:51 -0400

    fix

--- public/app/property-policy/module.js
+++ public/app/property-policy/module.js
@@ -1,433 +1,161 @@
 "use strict";
 
 
-angular.module('app.policy', ['ui.router','validation','ui.select'])
+angular.module('app.property-policy', ['ui.router','validation','ui.select'])
 
 
-angular.module('app.policy').config(function ($stateProvider, localStorageServiceProvider) {
-    localStorageServiceProvider.setPrefix('insu');
+angular.module('app.property-policy').config(function ($stateProvider, localStorageServiceProvider) {
+    localStorageServiceProvider.setPrefix('property-policy');
     
     $stateProvider
-        .state('app.policy', {
+        .state('app.property-policy', {
             abstract: true,
             data: {
-                title: '车险'
+                title: '财险'
             }
         })
-        .state('app.policy.new', {
-            url: '/policies/new',
+        .state('app.property-policy.new', {
+            url: '/property-policies/new',
             data: {
                 title: '保单录入'
             },
             views: {
                 "content@app": {
-                    controller: 'PolicyEditorController as vm',
-                    templateUrl: 'app/policy/views/policy.html'
+                    controller: 'PropertyPolicyEditorController as vm',
+                    templateUrl: 'app/property-policy/views/property-policy.html'
                 }
             }
         })
-        .state('app.policy.new1', {
-            url: '/policies/new1',
-            data: {
-                title: '保单录入(新版测试)'
-            },
-            views: {
-                "content@app": {
-                    controller: 'PolicyEditorController1 as vm',
-                    templateUrl: 'app/policy/views/policy1.html'
-                }
-            }
-        })
-        .state('app.policy.pay', {
-            url: '/policies/pay/:policyId',
+        .state('app.property-policy.pay', {
+            url: '/property-policies/pay/:policyId',
             data: {
                 title: '保单支付'
             },
             views: {
                 "content@app": {
-                    controller: 'PolicyEditorController as vm',
-                    templateUrl: 'app/policy/views/policy.html'
+                    controller: 'PropertyPolicyEditorController as vm',
+                    templateUrl: 'app/property-policy/views/property-policy.html'
                 }
             }
         })
-        .state('app.policy.pay1', {
-            url: '/policies/pay1/:policyId',
-            data: {
-                title: '保单支付'
-            },
-            views: {
-                "content@app": {
-                    controller: 'PolicyEditorController1 as vm',
-                    templateUrl: 'app/policy/views/policy1.html'
-                }
-            }
-        })
-        .state('app.policy.approve', {
-            url: '/policies/approve/:policyId',
+        .state('app.property-policy.approve', {
+            url: '/property-policies/approve/:policyId',
             data: {
                 title: '保单审核'
             },
             views: {
                 "content@app": {
-                    controller: 'PolicyEditorController as vm',
-                    templateUrl: 'app/policy/views/policy.html'
+                    controller: 'PropertyPolicyEditorController as vm',
+                    templateUrl: 'app/property-policy/views/property-policy.html'
                 }
             }
         })
-        .state('app.policy.approve1', {
-            url: '/policies/approve1/:policyId',
-            params: {policyId: null, ids: null},
-            data: {
-                title: '保单审核'
-            },
-            views: {
-                "content@app": {
-                    controller: 'PolicyEditorController1 as vm',
-                    templateUrl: 'app/policy/views/policy1.html'
-                }
-            }
-        })
-        .state('app.policy.view', {
-            url: '/policies/view/:policyId',
+        .state('app.property-policy.view', {
+            url: '/property-policies/view/:policyId',
             data: {
                 title: '保单查看'
             },
             views: {
                 "content@app": {
-                    controller: 'PolicyEditorController as vm',
-                    templateUrl: 'app/policy/views/policy.html'
+                    controller: 'PropertyPolicyEditorController as vm',
+                    templateUrl: 'app/property-policy/views/property-policy.html'
                 }
             }
         })
-        .state('app.policy.view1', {
-            url: '/policies/view1/:policyId',
-            data: {
-                title: '保单查看'
-            },
-            views: {
-                "content@app": {
-                    controller: 'PolicyEditorController1 as vm',
-                    templateUrl: 'app/policy/views/policy1.html'
-                }
-            }
-        })
-        .state('app.policy.check1', {
-            url: '/policies/check1/:policyId',
-            params: {policyId: null, ids: null},
-            data: {
-                title: '保单核对'
-            },
-            views: {
-                "content@app": {
-                    controller: 'PolicyEditorController1 as vm',
-                    templateUrl: 'app/policy/views/policy1.html'
-                }
-            }
-        })
-        .state('app.policy.to-be-reviewed', {
-            url: '/policies/to-be-reviewed',
-            data: {
-                title: '待审核保单'
-            },
-            views: {
-                "content@app": {
-                    controller: 'PolicyListController as vm',
-                    templateUrl: 'app/policy/views/policy-review-list.html'
-                }
-            },
-            resolve: {
-                srcipts: function(lazyScript){
-                    return lazyScript.register([
-                        'datatables',
-                        'datatables-bootstrap',
-                        'datatables-colvis',
-                        'datatables-tools',
-                        'datatables-responsive'
-                    ])
+        // .state('app.property-policy.to-be-reviewed', {
+        //     url: '/property-policies/to-be-reviewed',
+        //     data: {
+        //         title: '待审核保单'
+        //     },
+        //     views: {
+        //         "content@app": {
+        //             controller: 'PolicyListController as vm',
+        //             templateUrl: 'app/policy/views/policy-review-list.html'
+        //         }
+        //     },
+        //     resolve: {
+        //         srcipts: function(lazyScript){
+        //             return lazyScript.register([
+        //                 'datatables',
+        //                 'datatables-bootstrap',
+        //                 'datatables-colvis',
+        //                 'datatables-tools',
+        //                 'datatables-responsive'
+        //             ])
 
-                }
-            }
-        })
-        .state('app.policy.rejected', {
-            url: '/policies/rejected',
-            data: {
-                title: '被驳回保单'
-            },
-            views: {
-                "content@app": {
-                    controller: 'PolicyListController as vm',
-                    templateUrl: 'app/policy/views/policy-rejected-list.html'
-                }
-            },
-            resolve: {
-                srcipts: function(lazyScript){
-                    return lazyScript.register([
-                        'datatables',
-                        'datatables-bootstrap',
-                        'datatables-colvis',
-                        'datatables-tools',
-                        'datatables-responsive'
-                    ])
+        //         }
+        //     }
+        // })
+        // .state('app.property-policy.rejected', {
+        //     url: '/property-policies/rejected',
+        //     data: {
+        //         title: '被驳回保单'
+        //     },
+        //     views: {
+        //         "content@app": {
+        //             controller: 'PolicyListController as vm',
+        //             templateUrl: 'app/policy/views/policy-rejected-list.html'
+        //         }
+        //     },
+        //     resolve: {
+        //         srcipts: function(lazyScript){
+        //             return lazyScript.register([
+        //                 'datatables',
+        //                 'datatables-bootstrap',
+        //                 'datatables-colvis',
+        //                 'datatables-tools',
+        //                 'datatables-responsive'
+        //             ])
 
-                }
-            }
-        })
-        .state('app.policy.to-be-paid', {
-            url: '/policies/to-be-paid',
-            data: {
-                title: '待支付保单'
-            },
-            views: {
-                "content@app": {
-                    controller: 'PolicyListController as vm',
-                    templateUrl: 'app/policy/views/policy-list.html'
-                }
-            },
-            resolve: {
-                srcipts: function(lazyScript){
-                    return lazyScript.register([
-                        'datatables',
-                        'datatables-bootstrap',
-                        'datatables-colvis',
-                        'datatables-tools',
-                        'datatables-responsive'
-                    ])
+        //         }
+        //     }
+        // })
+        // .state('app.property-policy.to-be-paid', {
+        //     url: '/property-policies/to-be-paid',
+        //     data: {
+        //         title: '待支付保单'
+        //     },
+        //     views: {
+        //         "content@app": {
+        //             controller: 'PolicyListController as vm',
+        //             templateUrl: 'app/policy/views/policy-list.html'
+        //         }
+        //     },
+        //     resolve: {
+        //         srcipts: function(lazyScript){
+        //             return lazyScript.register([
+        //                 'datatables',
+        //                 'datatables-bootstrap',
+        //                 'datatables-colvis',
+        //                 'datatables-tools',
+        //                 'datatables-responsive'
+        //             ])
 
-                }
-            }
-        })
-        .state('app.policy.paid', {
-            url: '/policies/paid',
-            data: {
-                title: '已支付保单'
-            },
-            views: {
-                "content@app": {
-                    controller: 'PolicyListController as vm',
-                    templateUrl: 'app/policy/views/policy-list-paid.html'
-                }
-            },
-            resolve: {
-                srcipts: function(lazyScript){
-                    return lazyScript.register([
-                        'datatables',
-                        'datatables-bootstrap',
-                        'datatables-colvis',
-                        'datatables-tools',
-                        'datatables-responsive'
-                    ])
+        //         }
+        //     }
+        // })
+        // .state('app.property-policy.paid', {
+        //     url: '/property-policies/paid',
+        //     data: {
+        //         title: '已支付保单'
+        //     },
+        //     views: {
+        //         "content@app": {
+        //             controller: 'PolicyListController as vm',
+        //             templateUrl: 'app/policy/views/policy-list-paid.html'
+        //         }
+        //     },
+        //     resolve: {
+        //         srcipts: function(lazyScript){
+        //             return lazyScript.register([
+        //                 'datatables',
+        //                 'datatables-bootstrap',
+        //                 'datatables-colvis',
+        //                 'datatables-tools',
+        //                 'datatables-responsive'
+        //             ])
 
-                }
-            }
-        })
-        .state('app.policy.checked', {
-            url: '/policies/checked',
-            data: {
-                title: '已核对'
-            },
-            views: {
-                "content@app": {
-                    controller: 'PolicyListController as vm',
-                    templateUrl: 'app/policy/views/policy-list-checked.html'
-                }
-            },
-            resolve: {
-                srcipts: function(lazyScript){
-                    return lazyScript.register([
-                        'datatables',
-                        'datatables-bootstrap',
-                        'datatables-colvis',
-                        'datatables-tools',
-                        'datatables-responsive'
-                    ])
-
-                }
-            }
-        })
-        .state('app.policy.org-policy', {
-            abstract: true,
-            data: {
-                title: '车商车险'
-            }
-        })        
-        .state('app.policy.org-policy.import', {
-            url: '/policies/org-policies/import',
-            data: {
-                title: '车商保单批量导入'
-            },
-            views: {
-                "content@app": {
-                    controller: 'OrgPolicyImportController as vm',
-                    templateUrl: 'app/policy/views/org-policy-importer.html'
-                }
-            }
-        })
-        .state('app.policy.org-policy.to-be-paid', {
-            url: '/policies/org-policies/to-be-paid',
-            data: {
-                title: '待支付车商保单'
-            },
-            views: {
-                "content@app": {
-                    controller: 'OrgPolicyListController as vm',
-                    templateUrl: 'app/policy/views/org-policy-list.html'
-                }
-            }
-        })
-        .state('app.policy.org-policy.paid', {
-            url: '/policies/org-policies/paid',
-            data: {
-                title: '已支付车商保单'
-            },
-            views: {
-                "content@app": {
-                    controller: 'OrgPolicyListController as vm',
-                    templateUrl: 'app/policy/views/org-policy-list-paid.html'
-                }
-            }
-        })
-        .state('app.policy.image-policy', {
-            abstract: true,
-            data: {
-                title: '车商车险'
-            }
-        })    
-        .state('app.policy.image-policy.to-be-processed', {
-            url: '/policies/image-policies/to-be-processed',
-            data: {
-                title: '待录入保单照片'
-            },
-            views: {
-                "content@app": {
-                    controller: 'ImagePolicyListController as vm',
-                    templateUrl: 'app/policy/views/image-policy-list.html'
-                }
-            }
-        })
-        .state('app.policy.image-policy.processed', {
-            url: '/policies/image-policies/rocessed',
-            data: {
-                title: '已录入保单照片'
-            },
-            views: {
-                "content@app": {
-                    controller: 'ImagePolicyListController as vm',
-                    templateUrl: 'app/policy/views/image-policy-list.html'
-                }
-            }
-        })
-        .state('app.policy.dealer', {
-            abstract: true,
-            data: {
-                title: '渠道车险'
-            }
-        })
-        .state('app.policy.dealer.new', {
-            url: '/policies/dealer/new',
-            data: {
-                title: '保单录入'
-            },
-            views: {
-                "content@app": {
-                    controller: 'DealerPolicyEditorController as vm',
-                    templateUrl: 'app/policy/views/dealer-policy.html'
-                }
-            }
-        })
-        .state('app.policy.dealer.approve', {
-            url: '/policies/dealer/approve/:policyId',
-            data: {
-                title: '保单审核'
-            },
-            views: {
-                "content@app": {
-                    controller: 'DealerPolicyEditorController as vm',
-                    templateUrl: 'app/policy/views/dealer-policy.html'
-                }
-            }
-        })
-        .state('app.policy.dealer.view', {
-            url: '/policies/dealer/view/:policyId',
-            data: {
-                title: '保单查看'
-            },
-            views: {
-                "content@app": {
-                    controller: 'DealerPolicyEditorController as vm',
-                    templateUrl: 'app/policy/views/dealer-policy.html'
-                }
-            }
-        })
-        .state('app.policy.dealer.to-be-reviewed', {
-            url: '/policies/dealer/to-be-reviewed',
-            data: {
-                title: '待审核保单'
-            },
-            views: {
-                "content@app": {
-                    controller: 'DealerPolicyListController as vm',
-                    templateUrl: 'app/policy/views/dealer-policy-review-list.html'
-                }
-            },
-            resolve: {
-                srcipts: function(lazyScript){
-                    return lazyScript.register([
-                        'datatables',
-                        'datatables-bootstrap',
-                        'datatables-colvis',
-                        'datatables-tools',
-                        'datatables-responsive'
-                    ])
-
-                }
-            }
-        })
-        .state('app.policy.dealer.to-be-paid', {
-            url: '/policies/dealer/to-be-paid',
-            data: {
-                title: '待支付保单'
-            },
-            views: {
-                "content@app": {
-                    controller: 'DealerPolicyListController as vm',
-                    templateUrl: 'app/policy/views/dealer-policy-to-be-paid-list.html'
-                }
-            },
-            resolve: {
-                srcipts: function(lazyScript){
-                    return lazyScript.register([
-                        'datatables',
-                        'datatables-bootstrap',
-                        'datatables-colvis',
-                        'datatables-tools',
-                        'datatables-responsive'
-                    ])
-
-                }
-            }
-        })
-        .state('app.policy.dealer.paid', {
-            url: '/policies/dealer/paid',
-            data: {
-                title: '已支付保单'
-            },
-            views: {
-                "content@app": {
-                    controller: 'DealerPolicyListController as vm',
-                    templateUrl: 'app/policy/views/dealer-policy-paid-list.html'
-                }
-            },
-            resolve: {
-                srcipts: function(lazyScript){
-                    return lazyScript.register([
-                        'datatables',
-                        'datatables-bootstrap',
-                        'datatables-colvis',
-                        'datatables-tools',
-                        'datatables-responsive'
-                    ])
-
-                }
-            }
-        })
+        //         }
+        //     }
+        // })
 });
