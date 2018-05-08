d1d29fa89dc6cb9765a4c41811282c5df7bbfefd
Chun Wang <chunw@surveymonkey.com>
2018-05-07 23:51:02 -0400

    test page memory

--- public/app/policy/constrollers/PolicyListCtrl.js
+++ public/app/policy/constrollers/PolicyListCtrl.js
@@ -301,6 +301,7 @@ angular.module('app.policy').controller('PolicyListController', function (screen
                 localStorageService.set("rejected-currentPage", vm.currentPage);
             }
         }
+        console.log(vm.currentPage);
         vm.areAllSelected = false;
         PolicyService.searchPolicies(vm.currentPage, pageItems, vm.listType, vm.filterSettings, vm.fromDate, vm.toDate)
             .then(function (data) {
