703590ad13c25fc6a8cbf45eb76f6ab461971795
Chun Wang <chunw@surveymonkey.com>
2018-05-15 22:54:31 -0400

    text

--- routes/policies.js
+++ routes/policies.js
@@ -421,7 +421,7 @@ router.post('/search', function (req, res) {
 
   if (req.body.policyNoSearch != undefined && req.body.policyNoSearch !='') {
     let searchText = '/' + req.body.policyNoSearch + '/';
-    conditions['policy_no']= {$regex : searchText, $options : 'i'}
+    conditions['policy_no']= {$regex : req.body.policyNoSearch, $options : 'i'}
     // conditions.$or = [{ policy_no: searchText },{mandatory_policy_no: searchText}]
   }
 
